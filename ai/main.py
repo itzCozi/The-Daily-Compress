ce6e8ee33877b718fe45e6d63eb13bb306ae674c
Cooper <Cooperransom08@outlook.com>
2023-01-30 17:40:46 -0500

    Update to Version-1.0.3 Web

--- ai/main.py
+++ ai/main.py
@@ -1,11 +1,10 @@
-# Article-Writer Version.web-1.0.3
+# Article-Writer Version.web-BETA-1.0.3
 '''
 READ NOTES ON DESK FOR MORE INFO
 TODO: Add a way to automate the topic making process.
-TODO: Check if date logging error fixed if not fix it (https://www.programiz.com/python-programming/datetime/current-datetime)
+TODO: Now the topic prints when ran i can print the topic on top of the article
 TODO: Make a graph on front README demonstrating refresh process (https://github.com/ZeroMemoryEx/U-Boat/edit/master/README.md)
 TODO: Add log.html page where the logs.log is displayed in a iframe
-TODO: Compare to replit code and test this code
 '''
 
 # Imports
@@ -13,20 +12,22 @@ import os
 import random
 import time
 import openai
-from datetime import date
+import datetime
+from colorama import Fore, Style
 
 # Load API key
-openai.api_key = "NOOOOO NOT ANOTHER API LEAK"
+openai.api_key = "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"
 
 
 # Variables
 newFile = "data/article.txt"
+debug = True
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n"
 
 # Load topics into 'data' list
 topicFile = open("data/topics.txt", "r")
 data = topicFile.read()
 
-# when newline ('\n') is seen.
 data_into_list = data.split("\n")
 print(data_into_list)
 topicFile.close()
@@ -35,21 +36,26 @@ topicFile.close()
 # Functions
 def clear():
     # Log
-    with open("ai/logs.log", "w") as f:
-        f.write("Console cleared - AT: "+str(date.strftime))
-
+    with open("ai/logs.log", "a") as f:
+      f.write("Console cleared - AT: "+now)
+    if debug==True:
+      print(Fore.BLUE+"Console cleared - AT: "+now+Style.RESET_ALL)
+  
     clr = os.system('cls' if os.name in ('nt', 'dos') else 'clear')
     return clr
 
 def getIdea():
     # Log
-    with open("ai/logs.log", "w") as f:
-        f.write("Idea fetched - AT: "+str(date.strftime))
+    with open("ai/logs.log", "a") as f:
+        f.write("Idea fetched - AT: "+now)
+    if debug==True:
+      print(Fore.BLUE+"Idea fetched - AT: "+now+Style.RESET_ALL)
 
     idea = random.choice(data_into_list)
     formattedIdea = "Write an article about " + idea
-    clear()
-    print(formattedIdea)
+    clear() 
+    print("Prompt: "+formattedIdea)
+    print("Topic: "+idea)
     return formattedIdea
 
 def readFile():
@@ -59,19 +65,28 @@ def readFile():
 
 def write_request(getIdea):
     # Log
-    with open("ai/logs.log", "w") as f:
-        f.write("API reqeuest made - AT: "+str(date.strftime))
+    with open("ai/logs.log", "a") as f:
+      f.write("API reqeuest made - AT: "+now)
+    if debug==True:
+      print(Fore.BLUE+"API reqeuest made - AT: "+now+Style.RESET_ALL)
 
     # Request AI answer
-    response = openai.Completion.create(model="text-davinci-002", prompt=(getIdea), temperature=0.8, max_tokens=375)
+    response = openai.Completion.create(
+      model="text-davinci-002", 
+      prompt=(getIdea), 
+      temperature=0.8, 
+      max_tokens=400)
+    
     with open(newFile, "w") as file:
       file.write(str(response))
     return response
 
 def filter_response():
     # Log
-    with open("ai/logs.log", "w") as f:
-        f.write("Response filtered - AT: "+str(date.strftime))
+    with open("ai/logs.log", "a") as f:
+      f.write("Response filtered - AT: "+now)
+    if debug==True:
+      print(Fore.BLUE+"Response filtered - AT: "+now+Style.RESET_ALL)
 
     with open("data/article.txt", "w+") as f:
         proofread = openai.Edit.create(
