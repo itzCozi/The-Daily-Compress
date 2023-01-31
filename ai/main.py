8a007f7f4bfa6d57829fbb6a67a8bddbc9bb6470
Cooper <Cooperransom08@gmail.com>
2023-01-31 10:34:18 -0500

    Fixessss

--- ai/main.py
+++ ai/main.py
@@ -26,7 +26,6 @@ now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n"
 # Load topics into 'data' list
 topicFile = open("data/topics.txt", "r")
 data = topicFile.read()
-
 data_into_list = data.split("\n")
 print(data_into_list)
 topicFile.close()
@@ -34,81 +33,84 @@ topicFile.close()
 
 # Functions
 def clear():
-    # Log
-    with open("ai/logs.log", "a") as f:
-      f.write("Console cleared - AT: "+now)
-    if debug==True:
-      print(Fore.BLUE+"Console cleared - AT: "+now+Style.RESET_ALL)
-  
-    clr = os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-    return clr
+  # Log
+  with open("ai/logs.log", "a") as f:
+    f.write("Console cleared - AT: " + now)
+  if debug == True:
+    print(Fore.BLUE + "Console cleared - AT: " + now + Style.RESET_ALL)
+
+  clr = os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+  return clr
+
 
 def getIdea():
-    # Log
-    with open("ai/logs.log", "a") as f:
-      f.write("Idea fetched - AT: "+now)
-    if debug==True:
-      print(Fore.BLUE+"Idea fetched - AT: "+now+Style.RESET_ALL)
+  # Log
+  with open("ai/logs.log", "a") as f:
+    f.write("Idea fetched - AT: " + now)
+  if debug == True:
+    print(Fore.BLUE + "Idea fetched - AT: " + now + Style.RESET_ALL)
 
-    idea = random.choice(data_into_list)
-    formattedIdea = "Write an article about " + idea
-    clear() 
+  idea = random.choice(data_into_list)
+  formattedIdea = "Write an article about " + idea
+  clear()
+  print("Prompt: " + formattedIdea)
+  print("Topic: " + idea)
 
-    with open("data/topics.txt", "r") as f:
-      DELtopics = f.read().splitlines()
-    DELtopics.remove(idea)
+  with open("data/topics.txt", "r") as f:
+    DELtopics = f.read().splitlines()
+  DELtopics.remove(idea)
 
-    with open("data/topics.txt", "w") as f:
-      f.write('\n'.join(DELtopics))
-      
-    with open("ai/logs.log", "a") as f:
-        f.write("Topic Deleted - AT: "+now)
-        
-    if debug==True:
-        print(Fore.RED+"TOPIC DELETED"+Style.RESET_ALL)
-     
-    print("Prompt: "+formattedIdea)
-    print("Topic: "+idea)
-    return formattedIdea
+  with open("data/topics.txt", "w") as f:
+    f.write('\n'.join(DELtopics))
+
+  with open("ai/logs.log", "a") as f:
+    f.write("Topic Deleted - AT: " + now)
+
+  if debug == True:
+    print(Fore.RED + "TOPIC DELETED" + Style.RESET_ALL)
+
+  return formattedIdea
+
 
 def readFile():
   with open("data/article.txt", "w+") as f:
     for line in f:
       f.readline()
 
-def write_request(getIdea):
-    # Log
-    with open("ai/logs.log", "a") as f:
-      f.write("API reqeuest made - AT: "+now)
-    if debug==True:
-      print(Fore.BLUE+"API reqeuest made - AT: "+now+Style.RESET_ALL)
 
-    # Request AI answer
-    response = openai.Completion.create(
-      model="text-davinci-002", 
-      prompt=(getIdea), 
-      temperature=0.8, 
-      max_tokens=400)
-    
-    with open(newFile, "w") as file:
-      file.write(str(response))
-    return response
+def write_request(getIdea):
+  # Log
+  with open("ai/logs.log", "a") as f:
+    f.write("API reqeuest made - AT: " + now)
+  if debug == True:
+    print(Fore.BLUE + "API reqeuest made - AT: " + now + Style.RESET_ALL)
+
+  # Request AI answer
+  response = openai.Completion.create(model="text-davinci-002",
+    prompt=(getIdea),
+    temperature=0.8,
+    max_tokens=400)
+
+  with open(newFile, "w") as file:
+    file.write(str(response))
+  return response
+
 
 def filter_response():
-    # Log
-    with open("ai/logs.log", "a") as f:
-      f.write("Response filtered - AT: "+now)
-    if debug==True:
-      print(Fore.BLUE+"Response filtered - AT: "+now+Style.RESET_ALL)
+  # Log
+  with open("ai/logs.log", "a") as f:
+    f.write("Response filtered - AT: " + now)
+  if debug == True:
+    print(Fore.BLUE + "Response filtered - AT: " + now + Style.RESET_ALL)
 
-    with open("data/article.txt", "w+") as f:
-      proofread = openai.Edit.create(
+  with open("data/article.txt", "w+") as f:
+    proofread = openai.Edit.create(
       model="text-davinci-edit-001",
-      input= str(readFile()),
+      input=str(readFile()),
       instruction="Remove unnecessary characters")
-    
-      f.truncate(0)
-      f.write(str(proofread))
+
+    f.truncate(0)
+    f.write(str(proofread))
 
 
 # Main
